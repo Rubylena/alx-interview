1705868a8736a9437bd716680c7aea68b0711fea
Grace <graceffiong@gmail.com>
2023-02-16 12:25:35 +0100

    trial

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
+const async = require('async');
 
 // Retrieve the movie ID from the command line argument
 const movieId = process.argv[2];
 
 // Make a GET request to the Star Wars API to retrieve the movie information
-request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, (error, response, body) => {
+request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
   if (error) {
     console.error('Error making request:', error);
     return;
@@ -15,19 +16,32 @@ request(`https://swapi-api.hbtn.io/api/films/${movieId}/`, (error, response, bod
   // Parse the movie information from the response body
   const movie = JSON.parse(body);
 
-  // Loop through the list of character URLs and make a GET request for each one
-  movie.characters.forEach(characterUrl => {
-    request(characterUrl, (error, response, body) => {
-      if (error) {
-        console.error('Error making request:', error);
-        return;
-      }
+  // Create an array of functions to make a GET request for each character URL
+  const characterRequests = movie.characters.map(characterUrl => {
+    return function(callback) {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          callback(error);
+          return;
+        }
 
-      // Parse the character information from the response body
-      const character = JSON.parse(body);
+        // Parse the character information from the response body
+        const character = JSON.parse(body);
 
-      // Print the character name
-      console.log(character.name);
-    });
+        // Call the callback function with the character name
+        callback(null, character.name);
+      });
+    };
+  });
+
+  // Use the async library to make the requests in parallel
+  async.parallel(characterRequests, (error, characterNames) => {
+    if (error) {
+      console.error('Error making requests:', error);
+      return;
+    }
+
+    // Print the list of character names
+    console.log(characterNames.join('\n'));
   });
 });
