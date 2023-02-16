ce6cf8cf39b0a78dab64bf750b5c3c35a04a2476
Grace <graceffiong@gmail.com>
2023-02-16 12:28:15 +0100

    trial

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,47 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
-const async = require('async');
 
-// Retrieve the movie ID from the command line argument
 const movieId = process.argv[2];
 
-// Make a GET request to the Star Wars API to retrieve the movie information
-request(`https://swapi.dev/api/films/${movieId}/`, (error, response, body) => {
-  if (error) {
-    console.error('Error making request:', error);
-    return;
-  }
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
-  // Parse the movie information from the response body
-  const movie = JSON.parse(body);
+request(url, async (err, res, body) => {
+  err && console.log(err);
 
-  // Create an array of functions to make a GET request for each character URL
-  const characterRequests = movie.characters.map(characterUrl => {
-    return function(callback) {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          callback(error);
-          return;
-        }
+  const charactersArray = (JSON.parse(res.body).characters);
+  for (const character of charactersArray) {
+    await new Promise((resolve, reject) => {
+      request(character, (err, res, body) => {
+        err && console.log(err);
 
-        // Parse the character information from the response body
-        const character = JSON.parse(body);
-
-        // Call the callback function with the character name
-        callback(null, character.name);
+        console.log(JSON.parse(body).name);
+        resolve();
       });
-    };
-  });
-
-  // Use the async library to make the requests in parallel
-  async.parallel(characterRequests, (error, characterNames) => {
-    if (error) {
-      console.error('Error making requests:', error);
-      return;
-    }
-
-    // Print the list of character names
-    console.log(characterNames.join('\n'));
-  });
+    });
+  }
 });
