40f9345220d27d0063fddb1c2ea6986980879deb
Grace <graceffiong@gmail.com>
2023-03-01 09:49:21 +0100

    island perimeter interview

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" implementation"""
+
+
+def island_perimeter(grid):
+    perimeter = 0
+    m, n = len(grid), len(grid[0])
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j-1] == 1:
+                    perimeter -= 2
+    return perimeter
