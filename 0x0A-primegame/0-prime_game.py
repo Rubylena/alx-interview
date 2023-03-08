1efef31071e02da7a5de803dde8acd807dc49553
Grace <graceffiong@gmail.com>
2023-03-08 08:56:44 +0100

    executable

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -26,7 +26,6 @@ def isWinner(x, nums):
 
 def sieve(n):
     ''' Returns a list of prime numbers up to n '''
-
     primes = [True] * (n+1)
     primes[0] = primes[1] = False
     for i in range(2, int(n**0.5)+1):
